bd4351e3e3bcb08a4de773003a7ef6d0e2560af3
John Mac <johngrantuk@googlemail.com>
2018-08-05 16:18:27 +0100

    Deployed contracts to Rinkeby.

--- truffle.js
+++ truffle.js
@@ -2,6 +2,9 @@ require('babel-register')({
   ignore: /node_modules\/(?!zeppelin-solidity)/
 });
 require('babel-polyfill');
+var HDWalletProvider = require('truffle-hdwallet-provider');
+require('dotenv').load();
+var mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -11,6 +14,14 @@ module.exports = {
       host: "127.0.0.1",
       port: "8545",
       network_id: "*"
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + process.env.INFURA);
+      },
+      network_id: 4,
+      gasPrice: 20000000000, // 20 GWEI
+      gas: 3716887 // gas limit, set any number you want
     }
   }
 };
